f41bcbd0fc96550af4003ebbc469cbad762b1962
Miracle_Zero <ljy28501@163.com>
2021-09-08 15:06:32 +0800

    1015: 1不是质数！！

---
+++ advanced level/1015.cpp
@@ -0,0 +1,38 @@
+//1015 Reversible Primes
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+bool isPrime(int num){
+  for(int i=2;i<=sqrt(num);i++){
+    if(num%i==0)
+      return false;
+  }
+  return true;
+}
+
+int reverse(int num,int radix){
+  int ans=0;
+  while(num!=0){
+    ans=ans*radix+num%radix;
+    num=num/radix;
+  }
+  return ans;
+}
+
+int main(){
+  int N,D;
+  cin>>N;
+  while(N>=0){
+    cin>>D;
+    if(!isPrime(N))cout<<"No"<<endl;
+    else{
+      if(isPrime(reverse(N,D))){
+        cout<<"Yes"<<endl;
+      }else{
+        cout<<"No"<<endl;
+      }
+    }
+    cin>>N;
+  }
+}
